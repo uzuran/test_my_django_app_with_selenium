d5122fca998de6efe9e9e629a3849c1e6ef178cf
Uzuran <archloverlinux@gmail.com>
2024-07-14 17:53:36 +0200

    - Create separate files for base_test.py and my_base_page.py

--- tests/test_navbar.py
+++ tests/test_navbar.py
@@ -1,52 +1,37 @@
 """Tests for navbar."""
 import unittest
-from selenium.webdriver.chrome.options import Options
-from web_parts.nav_bar import NavBar  # Adjust this import according to your structure
+from tests.base_test import BaseTest
 
 
-class TestNavBar(unittest.TestCase):
+class TestNavBar(BaseTest):
     """Main test class for testing my navbar."""
-
-    @classmethod
-    def setUpClass(cls):
-        """Set up the WebDriver for all tests."""
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        cls.driver_path = r'C:\drivers\chromedriver-win64\chromedriver.exe'
-        cls.page = NavBar(cls.driver_path)
-
     def test_get_home_url(self):
         """Test if the get_home_url function retrieves the correct URL."""
         expected_url = "https://uzuran.pythonanywhere.com/"
-        actual_url = self.page.get_home_url()
+        actual_url = self.base_page.get_home_url()
         self.assertEqual(actual_url, expected_url,
                          f"Expected home URL '{expected_url}' but got '{actual_url}'.")
 
     def test_get_title_text(self):
         """Test if the get_title_text function retrieves the correct text."""
         expected_title = "Černopaščenko"  # Adjust this to the expected text
-        actual_text = self.page.get_title_text()
+        actual_text = self.base_page.get_title_text()
         self.assertEqual(actual_text, expected_title, f"Expected logo text '{expected_title}'"
                                                       f" but got '{actual_text}'.")
 
     def test_get_logo_text(self):
         """Test if the get_logo_text function retrieves the correct text."""
         expected_text = "Černopaščenko"  # Adjust this to the expected text
-        actual_text = self.page.get_logo_text()
+        actual_text = self.page_navbar.get_logo_text()
         self.assertEqual(actual_text, expected_text, f"Expected logo text '{expected_text}'"
                                                      f" but got '{actual_text}'.")
 
     def test_get_home_text(self):
         """Test if the get_home_text function retrieves the correct text."""
         expected_text = "Home"
-        actual_text = self.page.get_home_text()
+        actual_text = self.page_navbar.get_home_text()
         self.assertEqual(actual_text, expected_text, f"Expected home text '{expected_text}'"
                                                      f" but got '{actual_text}'.")
-
-    @classmethod
-    def tearDownClass(cls):
-        """Tear down the WebDriver after all tests."""
-        cls.page.close()
 
 
 if __name__ == "__main__":
