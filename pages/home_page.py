e8da63cc20af6ce089636b06adce5fd98dc0c212
Uzuran <archloverlinux@gmail.com>
2024-09-12 11:15:59 +0200

    - Change get_h1_logo_text() - Find logo text locator By.CSS_SELECTOR, "div.navbar h1"

--- pages/home_page.py
+++ pages/home_page.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 class HomePage(BasePage):
     def get_h1_logo_text(self):
         """
-        Get the text of the first <h1> element on the page.
+        Get the text of logo h1 text inside div navbar.
         """
-        h1_locator = (By.TAG_NAME, "h1")
+        h1_locator = (By.CSS_SELECTOR, "div.navbar h1")
         return self.find_element(h1_locator).text
